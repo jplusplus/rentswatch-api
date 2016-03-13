3c475cb0f4d12712c10dfb993f21154c1b8518dc
Nicolas Kayser-Bril <hi@nkb.fr>
2016-03-13 14:13:10 +0100

    added tiles to api desc

--- server/api/tile/tile.controller.js
+++ server/api/tile/tile.controller.js
@@ -4,20 +4,22 @@ var request = require('request');
 var response = require("../response");
 
 /**
- * @api {get} /api/tiles/:x/:y/:z Tile as geojson for a given location
- * @apiParam {Number} x Longitude of the tile
- * @apiParam {Number} y Latitude of the tile
+ * @api {get} /api/tiles/:z/:x/:y GeoJSON tile for a given ZXY
+ * @apiParam {Number} x X number of the tile for the given zoom level
+ * @apiParam {Number} y Y number of the tile for the given zoom level
  * @apiParam {Number} z Zoom level of the tile
  * @apiPermission Public
  * @apiGroup tiles
  * @apiName proxy
  *
- * @apiDescription Tiles as geojson
+ * @apiDescription GeoJSON tiles of the rent prices.
  *
  * @apiExample {curl} Example usage:
  *     curl -i http://api.rentswatch.com/api/tiles/10/489/379
  *
  * @apiSuccess {String} type  Type of the geojson.
+ * @apiSuccess {Array} features.geometry  The GeoJSON geometry.
+ * @apiSuccess {String} features.properties.price_per_sqm  Average price per square meter in Euro. The average price is the slope of the regression of each property's living space and total rent (including utilities).
  * @apiError 404 Tile not found
  * @apiErrorExample Response (example):
  *     HTTP/1.1 404 Not found
