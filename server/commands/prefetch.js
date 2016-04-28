447e7064dfd1a9eb4ec8fc2eaa60f89c361a5c4a
pirhoo <hello@pirhoo.com>
2016-04-28 16:21:24 +0200

    Improve prefetch

--- server/commands/prefetch.js
+++ server/commands/prefetch.js
@@ -5,20 +5,32 @@ require('../env')();
 
 var fs = require('fs'),
  async = require('async'),
-  path = require('path');
+  path = require('path'),
+  slug = require('slug'),
+  argv = require('yargs').usage('Usage: $0 -c [city]').alias('c', 'city').argv;
 // Cities collection
 var cities = require("../api/city/city.collection");
 
 // Collect promises for each city
 async.eachSeries(cities.toArray(), function(city, callback) {
-  return city.getStats().then(function(stats) {
-    var output = path.join(__dirname, '..', 'cache', 'cities', city.slug + '.json');
-    // Write stats for this city
-    fs.writeFileSync(output, JSON.stringify(stats, null, 2));
-    console.log(">> %s saved", city.name);
-    // Return the output file as promise result
-    callback(null, output);
-  }, callback).fail(callback);
+  if(!argv.city || slug(argv.city).toLowerCase() === slug(city.name).toLowerCase() ) {
+    return city.getStats().then(function(stats) {
+      var output = path.join(__dirname, '..', 'cache', 'cities', city.slug + '.json');
+      // Write stats for this city
+      fs.writeFile(output, JSON.stringify(stats, null, 2),  function(err) {
+        if(err) {
+          console.error(">> [ERROR] %s not saved: %s", city.name, err);
+          callback(err, null);
+        } else {
+          console.log(">> [SUCCESS] %s saved to %s", city.name, output);
+          // Return the output file as promise result
+          callback(null, output);
+        }
+      });
+    }, callback).fail(callback);
+  } else {
+    callback(null, null);
+  }
 // When all promises are resolved we stop the program
 }, function(err) {
   // Print out errors
